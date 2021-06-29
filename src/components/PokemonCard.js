bba2474ba13afba5ca93af1b70ebc5b8f650985b
Christy Lam <christylam@Christys-MBP.hitronhub.home>
2021-06-28 21:38:27 -0700

    centered all buttons and the card

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -26,64 +26,64 @@ const PokemonCard = (props) => {
     // let hpVal = statVals[0].props.children;
     // document.getElementById("hp").value= hpVal;
 
-    switch(types[0].props.children) {
-        case 'normal':
-            changeBackground('#FEF5E7');
-            break;
-        case 'fire':
-           changeBackground('#AC1E26');
-           break;
-        case 'fighting':
-            changeBackground('#D98880');
-            break;
-        case 'water':
-            changeBackground('#AED6F1');
-            break;
-        case 'flying':
-            changeBackground('#E8DAEF ');
-            break;
-        case 'grass':
-            changeBackground('#E2FFB5');
-            break;
-        case 'poison':
-            changeBackground('#AF7AC5');
-            break;
-        case 'electric':
-            changeBackground('#FFF1AE');
-            break;
-        case 'ground':
-            changeBackground('#AF601A');
-            break;
-        case 'psychic':
-            changeBackground('#FDB9DC');
-            break;
-        case 'rock':
-            changeBackground('#EDBB99');
-            break;
-        case 'ice':
-            changeBackground('#EBF5FB');
-            break;
-        case 'bug':
-            changeBackground('#E3FDB9 ');
-            break;
-        case 'dragon':
-            changeBackground('#B3D4FF');
-            break;
-        case 'ghost':
-            changeBackground('#AC80FF');
-            break;
-        case 'dark':
-            changeBackground('#979797');
-            break;
-        case 'steel':
-            changeBackground('#E4E4E4 ');
-            break;
-        case 'fairy':
-            changeBackground('#FFDFF3');
-            break;
-        default:
-            break; 
-    }
+    // switch(types[0].props.children) {
+    //     case 'normal':
+    //         changeBackground('#FEF5E7');
+    //         break;
+    //     case 'fire':
+    //        changeBackground('#AC1E26');
+    //        break;
+    //     case 'fighting':
+    //         changeBackground('#D98880');
+    //         break;
+    //     case 'water':
+    //         changeBackground('#AED6F1');
+    //         break;
+    //     case 'flying':
+    //         changeBackground('#E8DAEF ');
+    //         break;
+    //     case 'grass':
+    //         changeBackground('#E2FFB5');
+    //         break;
+    //     case 'poison':
+    //         changeBackground('#AF7AC5');
+    //         break;
+    //     case 'electric':
+    //         changeBackground('#FFF1AE');
+    //         break;
+    //     case 'ground':
+    //         changeBackground('#AF601A');
+    //         break;
+    //     case 'psychic':
+    //         changeBackground('#FDB9DC');
+    //         break;
+    //     case 'rock':
+    //         changeBackground('#EDBB99');
+    //         break;
+    //     case 'ice':
+    //         changeBackground('#EBF5FB');
+    //         break;
+    //     case 'bug':
+    //         changeBackground('#E3FDB9 ');
+    //         break;
+    //     case 'dragon':
+    //         changeBackground('#B3D4FF');
+    //         break;
+    //     case 'ghost':
+    //         changeBackground('#AC80FF');
+    //         break;
+    //     case 'dark':
+    //         changeBackground('#979797');
+    //         break;
+    //     case 'steel':
+    //         changeBackground('#E4E4E4 ');
+    //         break;
+    //     case 'fairy':
+    //         changeBackground('#FFDFF3');
+    //         break;
+    //     default:
+    //         break; 
+    // }
 
     function changeBackground(colour) {
         document.querySelector('.card').style.backgroundColor = colour;
@@ -98,7 +98,7 @@ const PokemonCard = (props) => {
             <h2>Height: {height} Weight: {weight}</h2>
             <h2>Abilities:{abilities}</h2>  
             <h2>Base Stats: {statVals}</h2>
-            <progress id="hp" value= "hp" max="100"></progress> 
+            {/* <progress id="hp" value= "hp" max="200"></progress>  */}
 
         </div>
     )
